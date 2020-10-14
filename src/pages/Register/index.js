9823f2687383cd4db876c186e8aab0aa19633c90
walyxxi <waliyulardy@gmail.com>
2020-10-14 08:51:50 +0700

    [ADD] restructure folders

---
+++ src/pages/Register/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./register.scss";
+
+const Register = () => {
+  return (
+    <div className="main-page">
+      <div className="left">
+        <p>Ilustrasi</p>
+      </div>
+      <div className="right">
+        <p>Form Register</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
