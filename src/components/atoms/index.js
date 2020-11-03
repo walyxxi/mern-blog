e1b77dcb8392fd84569987f09bb99151dd9aeb6b
walyxxi <waliyulardy@gmail.com>
2020-11-03 13:58:45 +0700

    [ADD] create and detail blog page

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -2,5 +2,7 @@ import Button from "./Button";
 import Gap from "./Gap";
 import Input from "./Input";
 import Link from "./Link";
+import TextArea from "./TextArea";
+import Upload from "./Upload";
 
-export { Input, Button, Gap, Link };
+export { Input, Button, Gap, Link, Upload, TextArea };
--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { RegisterBG } from "../../assets";
+import "./detailBlog.scss";
 
 const DetailBlog = () => {
   return (
-    <div>
-      <p>Content Detail Blog Page</p>
+    <div className="detail-blog-wrapper">
+      <img className="img-cover" src={RegisterBG} alt="thumd" />
+      <p className="blog-title">Title Blog</p>
+      <p className="blog-author">Author - Date Post</p>
+      <p className="blog-body">
+        Tempor nostrud dolore duis officia laboris amet cupidatat non. Id magna
+        voluptate commodo sit ut occaecat voluptate magna. Ad aliqua voluptate
+        ipsum non tempor ea do mollit. Lorem eiusmod excepteur cillum culpa
+        dolor proident sint nisi ex ex et labore culpa sit. Cupidatat
+        adipisicing exercitation minim mollit laboris adipisicing incididunt
+        proident esse amet et esse aliquip aliqua.
+      </p>
     </div>
   );
 };
 
-export default DetailBlog;
+export default DetailBlog; 
